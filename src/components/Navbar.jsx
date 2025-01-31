b335c878dd6869876d671187d031239acd80c355
raghavsingh05 <raghavsingh47336@gmail.com>
2025-02-01 03:40:49 +0530

    refactor: enhance navbar styling with backdrop blur and shadow effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,13 +43,13 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <div className='sm:hidden flex flex-1 justify-end items-center backdrop-blur-5xl'>
           <img src={toggle ? close : menu}
             alt='menu'
             className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => setToggle(!toggle)}
           />
-          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl `}>
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6  shadow-lg bg-neutral-950 backdrop-blur-xl border-neutral-900 border absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl `}>
             <ul className='list-none flex justify-end items-start flex-col gap-4'>
               {navLinks.map((Link) => (
                 <li
